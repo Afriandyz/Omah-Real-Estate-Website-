7d3cf0fe5bb3301339639fec3a015328e3c386ab
Afriandyz <araiz5021@gmail.com>
2024-10-18 17:19:31 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <section className="out">
+      <div className="in container px-3 py-2 mx-auto">
+        <h1>Hello World</h1>
+      </div>
+    </section>
+  );
+}
+
+export default App
