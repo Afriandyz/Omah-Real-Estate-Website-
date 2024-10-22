125c016f213e47d3be8eebc35127d02043071801
Afriandyz <araiz5021@gmail.com>
2024-10-22 16:26:49 +0800

    add review section

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,82 @@
+import reviewImage from "../../assets/ReviewImage/Review1.png";
+// Swiper React Components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
 const Review = () => {
   return (
-    <div className="">
-      Review
-    </div>
-  )
-}
+    <div className="wrapper">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={15}
+        centeredSlides={false}
+        autoplay={true}
+        className=""
+      >
+        <SwiperSlide>
+          <div className="wrapper2">
+            <img src={reviewImage} alt="" />
+            <span className="p-4 bg-white text-[#F58634] relative bottom-9">
+              Jone Doe
+            </span>
+            <h1 className="text-2xl w-[70%]">
+              Buying a house here is very cheap and high quality
+            </h1>
+          </div>
+        </SwiperSlide>
 
-export default Review
+        <SwiperSlide>
+          <div className="wrapper2">
+            <img src={reviewImage} alt="" />
+            <span className="p-4 bg-white text-[#F58634] relative bottom-9">
+              Jone Doe
+            </span>
+            <h1 className="text-2xl w-[70%]">
+              Buying a house here is very cheap and high quality
+            </h1>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="wrapper2">
+            <img src={reviewImage} alt="" />
+            <span className="p-4 bg-white text-[#F58634] relative bottom-9">
+              Jone Doe
+            </span>
+            <h1 className="text-2xl w-[70%]">
+              Buying a house here is very cheap and high quality
+            </h1>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="wrapper2">
+            <img src={reviewImage} alt="" />
+            <span className="p-4 bg-white text-[#F58634] relative bottom-9">
+              Jone Doe
+            </span>
+            <h1 className="text-2xl w-[70%]">
+              Buying a house here is very cheap and high quality
+            </h1>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="wrapper2">
+            <img src={reviewImage} alt="" />
+            <span className="p-4 bg-white text-[#F58634] relative bottom-9">
+              Jone Doe
+            </span>
+            <h1 className="text-2xl w-[70%]">
+              Buying a house here is very cheap and high quality
+            </h1>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Review;
---
+++ src/data/ReviewData.jsx
@@ -0,0 +1,17 @@
+export const reviewData = [
+  {
+    img: "https://freeimage.host/i/2FSCKQa",
+    name: "Jone Doe",
+    title: "Buying a house here is very cheap and high quality",
+  },
+  {
+    img: "https://imgur.com/a/GfIELpb",
+    name: "Angel Doe",
+    title: "Buying a house here is very cheap and high quality",
+  },
+  {
+    img: "https://i.ibb.co.com/Gc3bXJX/Review1.png",
+    name: "Jonh Doe",
+    title: "Buying a house here is very cheap and high quality",
+  },
+];
